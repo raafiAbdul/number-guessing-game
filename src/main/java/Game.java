d6e0bd6b87106803b35e4f5408d208e2a68b9645
raafiAbdul <rafrafyunos@gmail.com>
2026-07-26 05:27:28 +0800

    added welcome message

---
+++ src/main/java/Game.java
@@ -0,0 +1,8 @@
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I'm thinking of a number between 1 and 100");
+        System.out.println("You have 3-10 chances (depending on the difficulty)\nto guess the correct number");
+
+    }
+}
