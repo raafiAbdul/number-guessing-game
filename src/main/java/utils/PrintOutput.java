7a9fb4bc5dccd3d683a896f0bcdf097eaa1e304e
raafiAbdul <rafrafyunos@gmail.com>
2026-07-26 05:34:41 +0800

    designated welcome message to PrintOutput class

--- src/main/java/utils/PrintOutput.java
+++ src/main/java/utils/PrintOutput.java
@@ -1,4 +1,14 @@
 package utils;
 
 public class PrintOutput {
+    public static void printWelcomeMessage() {
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I'm thinking of a number between 1 and 100");
+        System.out.println("You have 3-10 chances (depending on the difficulty)\nto guess the correct number");
+
+        System.out.println("\nPlease select the difficulty level:");
+        System.out.println("1. Easy (10 chances)");
+        System.out.println("2. Medium (5 chances)");
+        System.out.println("3. Hard (3 chances)");
+    }
 }
